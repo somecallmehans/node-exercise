3ebe209cc4c47f3319e3ed880e2b1d934c89d066
Christian Anderson <somecallmehans@gmail.com>
2021-09-24 12:54:32 -0400

    added pwd and ls

--- bash.js
+++ bash.js
@@ -1,13 +1,20 @@
+const pwdCommand = require('./pwd');
+const lsCommand = require('./ls');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
   if(cmd === 'pwd'){
-    console.log(process.cwd());
+    pwdCommand();
+  }
+
+  if(cmd === 'ls'){
+    lsCommand();
   }
 
 
-  process.stdout.write('You typed: ' +cmd);
-  process.stdout.write('\nprompt > ');
+  //process.stdout.write('You typed: ' +cmd);
+  //process.stdout.write('\nprompt > ');
 })
---
+++ pwd.js
@@ -0,0 +1,5 @@
+module.exports = () => {
+  console.log(process.cwd());
+  process.stdout.write('\nprompt > ');
+}
+
